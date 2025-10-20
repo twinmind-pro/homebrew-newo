490a757ed08698b75b046b9099fbe1d93fa121e1
Eugene <e.panin@gmail.com>
2025-10-20 19:14:50 +0200

    Update NewoTool to version 0.3.2

--- Formula/newo-tool.rb
+++ Formula/newo-tool.rb
@@ -1,17 +1,17 @@
 class NewoTool < Formula
   desc "A CLI tool for Newo"
   homepage "https://github.com/twinmind-pro/newo-tool"
-  version "0.3.0"
+  version "0.3.2"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/twinmind-pro/newo-tool/releases/download/v0.3.0/newo-v0.3.0-darwin-amd64.tar.gz"
-    sha256 "1de32db8c473ca2e673e0544021e2c4ad8998da3e7f1e0c874ed764c7e91e125"
+    url "https://github.com/twinmind-pro/newo-tool/releases/download/v0.3.2/newo-v0.3.2-darwin-amd64.tar.gz"
+    sha256 "16051255f1902a0d2717e717dfba4955d6984e69efae2fb5a0efa49807c74df5"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/twinmind-pro/newo-tool/releases/download/v0.3.0/newo-v0.3.0-darwin-arm64.tar.gz"
-    sha256 "4bb74a9ee37f12115084354cf85993e613e53d7c4a13db9b59073148c17479f3"
+    url "https://github.com/twinmind-pro/newo-tool/releases/download/v0.3.2/newo-v0.3.2-darwin-arm64.tar.gz"
+    sha256 "dc68c73dfd3c3b46002502b1bbdb3fa6c22b5ac8b42d70e9ae43da0d877edc54"
   end
 
   def install
